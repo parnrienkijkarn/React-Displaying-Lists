970e14b4a29991dcc53ee14243180ea919b66ca2
Paul Ren <pause.forever@gmail.com>
2022-09-21 15:47:09 +0700

    add initial files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <section className="movie-list-section">
+        {/* Render Movie Lists Here */}
+      </section>
+      <section className="team-list-section">
+        {/* Render Team List Here */}
+      </section>
+    </div>
+  );
+}
+
+export default App;
